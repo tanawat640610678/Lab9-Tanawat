8a621139661d73078e4eef5684a571830491c179
tanawat640610678 <thanawat_chai@cmu.ac.th>
2021-12-06 22:16:34 +0700

    Lab9_3.cpp Complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,11 +1,55 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   int age,height,property;
+   cout << "Enter your age: ";
+   cin >> age;
+   
+    if(age <= 20){
+        cout << "Enter your height: ";
+        cin >> height;
+
+                 if (height < 160 ){
+                    
+                    cout << "Your status = UNFRIEND";
+                }
+       
+                else { if (height < 175 && height >= 160)
+                        cout <<  "Your status = FRIEND";
+
+                        else{ cout << "Enter your property: ";
+                              cin >> property;
+
+                              if (property > 69000000)
+                                cout << "Your status = MARRIED";
+                              else cout << "Your status = ONE-NIGHT-STAND";
+
+                        }
+         
+                }
+              
+    }
+             
+    
+    else{
+        if(age < 30){
+            cout << "Enter your property: ";
+            cin >> property;
+
+                 if(property > 10000000)
+                      cout << "Your status = BEST FRIEND";
+                 else cout << "Your status = UNFRIEND";
+            
+        }
+        else cout << "Your status = UNFRIEND";
+
+
+    }
+
+            
+    return 0;
+  
+
+} 
